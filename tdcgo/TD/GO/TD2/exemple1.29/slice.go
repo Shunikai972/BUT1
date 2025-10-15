8184051f8cdedcaad3fed36bc831a0806dbcd0db
Shunikai972 <noammsakni@gmail.com>
2025-10-15 17:09:34 +0200

    V0.12

--- tdcgo/TD/GO/TD2/exemple1.29/slice.go
+++ tdcgo/TD/GO/TD2/exemple1.29/slice.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-func f(t [4]int) {
+func f(t []int) {
 	for i := 0; i < len(t); i++ {
 		t[i] = 0
 	}
 }
-
 func main() {
-	var tab [4]int = [4]int{1, 2, 3, 4}
+	var tab []int = []int{1, 2, 3, 4}
 	fmt.Println(tab)
 	f(tab)
 	fmt.Println(tab)
