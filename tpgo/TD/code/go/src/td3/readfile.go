ece669ddb9f2bd62887d2156366bb9a6611080d0
Shunikai972 <noammsakni@gmail.com>
2025-11-14 16:21:09 +0100

    V0.3

--- tpgo/TD/code/go/src/td3/readfile.go
+++ tpgo/TD/code/go/src/td3/readfile.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var filePath string = "test"
+	var filePath string = "/var/home/E256190S/reseau/Perso/Bureau/BUT1/tpgo/TD/code/go/src/td3/table.txt"
 	var data []byte
 	var err error
 	data, err = ioutil.ReadFile(filePath)
